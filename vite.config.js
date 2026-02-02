e102d56080d14f1d7dec863947312f39147ad92e
audreyau <auau@oregonstate.edu>
2026-02-01 19:52:59 -0800

    Update Vite config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,11 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   base: '/desktop-portfolio/',
   build: {
-    outDir: 'dist'
+    rollupOptions: {
+      external: ['fsevents']
+    }
+  },
+  optimizeDeps: {
+    exclude: ['fsevents']
   }
 })
